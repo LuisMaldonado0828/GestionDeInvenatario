f4a8026d6839ffbffb418b350f9bceacac90932d
Luis Maldonado <lm271022@gmail.com>
2025-08-07 19:37:59 -0500

    Agregar atributos y metodos basicos en las clase Producto y Inventario

--- Inventario.java
+++ Inventario.java
@@ -1,4 +1,14 @@
 package SistemaDeGestionInventario;
 
+import java.util.ArrayList;
+
 public class Inventario {
+    ArrayList <Producto> productos = new ArrayList<>();
+
+    public void agregarProductos(){}
+    public void consultarInventario(){}
+    public void actualizarProducto(){}
+    public void eliminarProducto(){}
+    public void calcularValorTotal(){}
+
 }
--- Producto.java
+++ Producto.java
@@ -1,4 +1,65 @@
 package SistemaDeGestionInventario;
 
 public class Producto {
+    private String Id;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+
+    public Producto(String Id,String nombre, int cantidad, double precio){
+        this.setId(Id);
+        this.setNombre(nombre);
+        this.setCantidad(cantidad);
+        this.setPrecio(precio);
+    }
+
+    public String getId(){
+        return Id;
+    }
+
+    public void setId(String id) {
+        if(id == null || id.trim().isEmpty()){
+            throw new IllegalArgumentException("Debe de contener un ID del producto valido");
+        }else {
+            this.Id = id;
+        }
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        if(nombre == null || nombre.trim().isEmpty()){
+            throw new IllegalArgumentException("Debe de contener un nombre el producto.");
+        }else {
+            this.nombre = nombre;
+        }
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        if(cantidad <= 0){
+            throw new IllegalArgumentException("Debe de tener una cantidad valida mayor a 0 ");
+        }else{
+            this.cantidad = cantidad;
+        }
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        if(precio <= 0){
+            throw new IllegalArgumentException("Debe de tener un precio mayor a 0 ");
+        }else{
+            this.precio = precio;
+        }
+    }
 }
